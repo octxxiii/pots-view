c0ea30c448f8cb672c7023d15d3f397906a364a5
minjunkim <kimmj@ji-in.com>
2024-02-28 18:00:43 +0900

    sidebar

--- src/components/DNAView/index.tsx
+++ src/components/DNAView/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   Box,
@@ -10,6 +10,14 @@ import {
   TableContainer,
   TableRow,
   Paper,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  TextField,
+  SelectChangeEvent,
 } from "@mui/material";
 
 type DnaViewProps = {
@@ -33,13 +41,70 @@ const tempData = [
   },
 ];
 
-const DnaView: React.FC<DnaViewProps> = ({ isOpen }) => {
+const DnaView: React.FC<{ isOpen: boolean }> = ({ isOpen }) => {
+  const [filter, setFilter] = useState<string>("");
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const handleFilterChange = (event: SelectChangeEvent) => {
+    setFilter(event.target.value);
+  };
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <Container maxWidth="lg">
       <Box my={4}>
-        <Typography variant="h4" gutterBottom style={{ color: "black" }}>
+        <Typography variant="h4" gutterBottom sx={{ color: "black" }}>
           악성코드 DNA 분석 결과 시각화
         </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            marginBottom: 2,
+          }}
+        >
+          <FormControl
+            variant="filled"
+            sx={{ minWidth: 200, maxHeight: 100, color: "black" }}
+          >
+            <InputLabel id="demo-simple-select-filled-label">
+              필터링 대상
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-filled-label"
+              id="demo-simple-select-filled"
+              value={filter}
+              onChange={handleFilterChange}
+              sx={{ color: "black", ".MuiSvgIcon-root": { color: "black" } }} // MUI icon color
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="id">분석요청ID</MenuItem>
+              <MenuItem value="hash">해시</MenuItem>
+              {/* 추가 필터링 옵션 */}
+            </Select>
+          </FormControl>
+          <TextField
+            id="filled-basic"
+            label="필터링 조건"
+            variant="filled"
+            sx={{
+              color: "black",
+              ".MuiInputBase-root": { color: "black" },
+              marginLeft: 2,
+            }} // Text field styles
+          />
+          <Checkbox
+            checked={checked}
+            onChange={handleCheckboxChange}
+            sx={{ color: "black", "&.Mui-checked": { color: "black" } }} // Checkbox styles
+          />
+        </Box>
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
@@ -51,25 +116,7 @@ const DnaView: React.FC<DnaViewProps> = ({ isOpen }) => {
                 <TableCell align="center">추가 정보</TableCell>
               </TableRow>
             </TableHead>
-            <TableBody>
-              {tempData.length > 0 ? (
-                tempData.map((data) => (
-                  <TableRow key={data.id}>
-                    <TableCell align="center">{data.id}</TableCell>
-                    <TableCell align="center">{data.hash}</TableCell>
-                    <TableCell align="center">{data.fileName}</TableCell>
-                    <TableCell align="center">{data.groupName}</TableCell>
-                    <TableCell align="center">{data.extraInfo}</TableCell>
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell align="center" colSpan={5}>
-                    데이터가 없습니다.
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
+            <TableBody>{/* 테이블 바디 데이터 */}</TableBody>
           </Table>
         </TableContainer>
       </Box>
